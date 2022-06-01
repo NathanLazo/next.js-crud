064f32944134adb9c6d83916f1ef68f344e638dc
Nathan Lazo <nathanlazo@outlook.com>
2022-05-31 21:56:52 -0600

    [UPDATE] Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,7 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
 const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+
 ];
 
 export default function Home() {
@@ -22,34 +19,7 @@ export default function Home() {
 
                         <Popover>
                             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-                                <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                                    <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                                        <div className="flex items-center justify-between w-full md:w-auto">
-                                            <Link href="/login">
-                                                <>
-                                                    <span className="sr-only">Workflow</span>
-                                                    <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" />
-                                                </>
-                                            </Link>
-                                            <div className="-mr-2 flex items-center md:hidden">
-                                                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                                    <span className="sr-only">Open main menu</span>
-                                                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                                                </Popover.Button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                                        {navigation.map((item) => (
-                                            <Link key={`mobile-${item.name}`} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                                                {item.name}
-                                            </Link>
-                                        ))}
-                                        <Link href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                            Log in
-                                        </Link>
-                                    </div>
-                                </nav>
+
                             </div>
 
                             <Transition as={Fragment} enter="duration-150 ease-out" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="duration-100 ease-in" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
@@ -89,13 +59,17 @@ export default function Home() {
                                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
                                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                     <div className="rounded-md shadow">
-                                        <Link href="/login" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                                            Get started
+                                        <Link href="/login" >
+                                            <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                                                Get started
+                                            </a>
                                         </Link>
                                     </div>
                                     <div className="mt-3 sm:mt-0 sm:ml-3">
-                                        <Link href="/login" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                                            Live demo
+                                        <Link href="/login" >
+                                            <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                                               Live demo 
+                                            </a>
                                         </Link>
                                     </div>
                                 </div>
