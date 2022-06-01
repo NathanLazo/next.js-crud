b3cdee7ba7b8e4b3e011ab15904329fb0b4d3ca1
Nathan Lazo <nathanlazo@outlook.com>
2022-05-31 22:04:38 -0600

    [FIX] Icon error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { XIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
 const navigation = [
